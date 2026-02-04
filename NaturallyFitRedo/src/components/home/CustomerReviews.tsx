3ae2db001d449264155c7ffcfaf8a614950553c7
Bill Stein <billsteinm03@gmail.com>
2026-02-04 13:19:32 -0400

    Fix Google Reviews logo filename: Google-reviews-logo.jpg

--- NaturallyFitRedo/src/components/home/CustomerReviews.tsx
+++ NaturallyFitRedo/src/components/home/CustomerReviews.tsx
@@ -159,7 +159,7 @@ export default function CustomerReviews({
         {/* Google Reviews Badge */}
         <div className="flex justify-center mt-8">
           <Image
-            src={wpAsset("google-reviews_logo.jpg")}
+            src={wpAsset("Google-reviews-logo.jpg")}
             alt="Google Reviews"
             width={225}
             height={92}
